33f60f4900c04d25a2b8de6488897dfe65081422
Nianyu Shen <nianyu@spectrocloud.com>
2024-03-27 07:45:09 +0000

    add create command
    Signed-off-by: Nianyu Shen <nianyu@spectrocloud.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::{arg, Command};
+
+fn cli() -> Command {
+    Command::new("balaeno")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new("create")
+                .about("create container")
+                .arg(arg!([bundle]))
+                .arg(arg!([id]))
+                .arg_required_else_help(true),
+        )
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("create", sub_matches)) => {
+            let bundle = sub_matches
+                .get_one::<String>("bundle")
+                .map(|s| s.as_str())
+                .unwrap();
+            let id = sub_matches
+                .get_one::<String>("id")
+                .map(|s| s.as_str())
+                .unwrap();
+            println!("create container: bundle: {:?}, id: {:?}", bundle, id);
+        }
+        _ => unreachable!(), // If all subcommands are defined above, anything else is unreachable!()
+    }
 }

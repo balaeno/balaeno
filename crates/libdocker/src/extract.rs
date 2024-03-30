17555d5990c0a0379c8b60d66cee370e50b54d20
Nianyu Shen <nianyu@spectrocloud.com>
2024-03-30 09:01:06 +0000

    untar after downloading image
    Signed-off-by: Nianyu Shen <nianyu@spectrocloud.com>

--- crates/libdocker/src/extract.rs
+++ crates/libdocker/src/extract.rs
@@ -1,19 +1,23 @@
 use anyhow::Result;
 use bollard::Docker;
+use flate2::read::GzDecoder;
 use futures_util::{future::ready, StreamExt};
 use std::{
     fs::{self, File},
     io::Write,
     path::Path,
 };
+use tar::Archive;
 
 pub async fn extract(image_url: String, path: String) -> Result<()> {
     let docker = Docker::connect_with_local_defaults().expect("failed to connect to docker");
     let image = docker.export_image(&image_url);
 
     fs::create_dir_all(&path).expect("failed to create directory");
-    let temp_file_path = Path::new(&path).join("image.tar");
-    let mut temp_file = File::create(temp_file_path).expect("failed to create temp file");
+    let p = Path::new(&path);
+    let temp_file_path = p.join("image.tar");
+
+    let mut temp_file = File::create(temp_file_path.clone()).expect("failed to create temp file");
     // Shouldn't load the whole file into memory, stream it to disk instead
     image
         .for_each(move |data| {
@@ -22,5 +26,14 @@ pub async fn extract(image_url: String, path: String) -> Result<()> {
             ready(())
         })
         .await;
+
+    // untar the image.tar
+    let temp_file = File::open(temp_file_path.clone()).expect("failed to open temp file");
+    let tar = GzDecoder::new(temp_file);
+    let mut archive = Archive::new(tar);
+    archive.unpack(&path).expect("failed to unpack image");
+
+    // remove the temp file
+    fs::remove_file(temp_file_path).expect("failed to remove temp file");
     Ok(())
 }

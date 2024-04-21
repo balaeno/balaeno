b19e136aa237be50a9ee76d44506ec53da3ed039
Nianyu Shen <xiaoyu9964@gmail.com>
2024-04-20 19:19:52 -0700

    implement mount_rootfs
    Signed-off-by: Nianyu Shen <xiaoyu9964@gmail.com>

--- libruntime/src/fs.rs
+++ libruntime/src/fs.rs
@@ -2,8 +2,7 @@ use std::{fs::create_dir_all, path::Path};
 
 use anyhow::{anyhow, Result};
 use nix::{
-    mount::umount2,
-    mount::MntFlags,
+    mount::{mount, umount2, MntFlags, MsFlags},
     unistd::{chdir, pivot_root},
 };
 
@@ -14,6 +13,35 @@ pub fn abs_path(path: &str) -> Option<String> {
     can_path.into_os_string().into_string().ok()
 }
 
+pub fn mount_rootfs(root: &Path) -> Result<()> {
+    match mount(
+        None::<&str>,
+        "/",
+        None::<&str>,
+        MsFlags::MS_PRIVATE | MsFlags::MS_REC,
+        None::<&str>,
+    ) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(anyhow!("failed to mount rootfs: {:?}", e));
+        }
+    }
+
+    match mount::<Path, Path, str, str>(
+        Some(&root),
+        &root,
+        None::<&str>,
+        MsFlags::MS_BIND | MsFlags::MS_REC,
+        None::<&str>,
+    ) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(anyhow!("failed to bind mount rootfs: {:?}", e));
+        }
+    }
+
+    Ok(())
+}
 pub fn pivot_rootfs(root: &Path) -> Result<()> {
     match chdir(root) {
         Ok(_) => {}
